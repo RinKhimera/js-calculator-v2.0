1a7083e009f2e5cd0be34c7fe08cdcb73828185e
Samuel Pokam <dixiades@gmail.com>
2023-01-04 11:32:00 +0100

    CSS updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
-import { Navbar } from "./components/Navbar";
+import React from "react";
+import Calculator from "./components/Calculator";
 
 function App() {
-  const [darkTheme, setDarkTheme] = useState(false);
-
   return (
-    <div className={darkTheme ? "dark" : null}>
-      <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
-        <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
-      </div>
+    <div>
+      <Calculator />
     </div>
   );
 }
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,249 +1,259 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Cal = () => {
-  const [cal, setCal] = useState("");
-  const [result, setResult] = useState("");
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayResult: "0",
+      isDarkMode: false,
+    };
+  }
 
-  const updateCal = (e) => {
-    setCal(cal.concat(e.target.name));
-  };
-
-  const clear = () => {
-    setCal("");
-    setResult("");
-  };
-
-  const results = () => {
-    try {
-      setResult(eval(cal).toString());
-    } catch {
-      setResult("Error");
+  calcNumbers(val) {
+    if (!("x/+%".indexOf(val) > -1 && this.state.displayResult === "0")) {
+      if (val === "C") {
+        this.setState({ displayResult: "0" });
+      } else if (val === "<") {
+        if (this.state.displayResult !== "0") {
+          if (this.state.displayResult.length === 1) {
+            this.setState({
+              displayResult:
+                this.state.displayResult.slice(
+                  0,
+                  this.state.displayResult.length - 1
+                ) + "0",
+            });
+          } else {
+            this.setState({
+              displayResult: this.state.displayResult.slice(
+                0,
+                this.state.displayResult.length - 1
+              ),
+            });
+          }
+        }
+      } else {
+        if (
+          ("x/+%-".indexOf(val) > -1 &&
+            "x/+%-".indexOf(this.state.displayResult.slice(-1)) > -1) ||
+          (this.state.displayResult === "0" && val !== ".") ||
+          (this.state.displayResult.slice(-1) === "." && val === ".")
+        ) {
+          if (
+            val === "-" &&
+            "x/".indexOf(this.state.displayResult.slice(-1)) > -1
+          ) {
+            this.setState({ displayResult: this.state.displayResult + val });
+          } else {
+            if (
+              "x/+%-".indexOf(
+                this.state.displayResult.charAt(
+                  this.state.displayResult.length - 2
+                )
+              ) > -1
+            ) {
+              this.setState({
+                displayResult:
+                  this.state.displayResult.slice(
+                    0,
+                    this.state.displayResult.length - 2
+                  ) + val,
+              });
+            } else {
+              this.setState({
+                displayResult:
+                  this.state.displayResult.slice(
+                    0,
+                    this.state.displayResult.length - 1
+                  ) + val,
+              });
+            }
+          }
+        } else if (
+          "x/+-%".indexOf(this.state.displayResult.slice(-1)) > -1 &&
+          val === "."
+        ) {
+          this.setState({ displayResult: this.state.displayResult + "0." });
+        } else {
+          let last = this.state.displayResult.split(/x|\/|\+|-|%/);
+          if (!(last[last.length - 1].indexOf(".") > -1 && val === ".")) {
+            this.setState({ displayResult: this.state.displayResult + val });
+          }
+        }
+      }
     }
-  };
+  }
 
-  return (
-    <div class="mx-auto overflow-hidden mt-10 shadow-lg mb-2 bg-[#000000] border rounded-lg lg:w-2/6 md:w-3/6 sm:w-4/6">
-      <div class="">
-        <div class="p-5 text-white text-center text-3xl bg-[#04387A]-900">
-          Calculator
-        </div>
-        <div class="pt-16 p-5 pb-0 text-white text-right text-3xl bg-[#000000]">
-          {cal || 0}
-        </div>
-        <div class="p-5 text-white text-right text-3xl bg-[#000000]">
-          <span class="text-orange-500">{result || ""}</span>
-        </div>
+  evalResult() {
+    let result;
+    // eslint-disable-next-line
+    result = eval(this.state.displayResult.replace(/x/g, "*"));
+    if (result % 1 !== 0) {
+      result = parseFloat(result.toFixed(4));
+    }
+    this.setState({ displayResult: result.toString() });
+  }
 
-        <div class="flex items-stretch bg-[#000000] h-24">
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-[#505050] justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="%"
-              onClick={updateCal}
-            >
-              %
-            </button>
-          </div>
+  changeTheme() {
+    this.setState({ isDarkMode: !this.state.isDarkMode });
+  }
 
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-[#505050] justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="("
-              onClick={updateCal}
-            >
-              (
-            </button>
-          </div>
+  render() {
+    return (
+      <div className={this.state.isDarkMode ? "theme-black" : ""}>
+        <div className="container">
+          <form>
+            <div className="display">
+              <div className="theme" onClick={() => this.changeTheme()}>
+                <i className="fas fa-moon"></i>
+              </div>
+              <input
+                type="text"
+                id="display"
+                readOnly
+                placeholder="0"
+                value={this.state.displayResult}
+              />
+            </div>
+            <div className="buttons">
+              <div className="row">
+                <input
+                  type="button"
+                  id="clear"
+                  className="text-primary"
+                  value="C"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="divide"
+                  className="text-primary"
+                  value="/"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="multiply"
+                  className="text-primary"
+                  value="x"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  className="text-primary backspace"
+                  value="<"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+              </div>
+              <div className="row">
+                <input
+                  type="button"
+                  id="seven"
+                  value="7"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="eight"
+                  value="8"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="nine"
+                  value="9"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="subtract"
+                  className="text-primary"
+                  value="-"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+              </div>
 
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-[#505050] justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name=")"
-              onClick={updateCal}
-            >
-              )
-            </button>
-          </div>
+              <div className="row">
+                <input
+                  type="button"
+                  id="four"
+                  value="4"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="five"
+                  value="5"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="six"
+                  value="6"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="add"
+                  className="text-primary"
+                  value="+"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+              </div>
 
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-orange-500 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="/"
-              onClick={updateCal}
-            >
-              ÷
-            </button>
-          </div>
-        </div>
+              <div className="row">
+                <input
+                  type="button"
+                  id="one"
+                  value="1"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="two"
+                  value="2"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="three"
+                  value="3"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  className="equal"
+                  id="equals"
+                  value="="
+                  onClick={() => this.evalResult()}
+                />
+              </div>
 
-        <div class="flex items-stretch bg-[#000000] h-24">
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="7"
-              onClick={updateCal}
-            >
-              7
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="8"
-              onClick={updateCal}
-            >
-              8
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="9"
-              onClick={updateCal}
-            >
-              9
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-orange-500 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="*"
-              onClick={updateCal}
-            >
-              ×
-            </button>
-          </div>
-        </div>
-
-        <div class="flex items-stretch bg-[#000000] h-24">
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="4"
-              onClick={updateCal}
-            >
-              4
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="5"
-              onClick={updateCal}
-            >
-              5
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="6"
-              onClick={updateCal}
-            >
-              6
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-orange-500 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="-"
-              onClick={updateCal}
-            >
-              -
-            </button>
-          </div>
-        </div>
-
-        <div class="flex items-stretch bg-[#000000] h-24">
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="1"
-              onClick={updateCal}
-            >
-              1
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="2"
-              onClick={updateCal}
-            >
-              2
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="3"
-              onClick={updateCal}
-            >
-              3
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-orange-500 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="+"
-              onClick={updateCal}
-            >
-              +
-            </button>
-          </div>
-        </div>
-
-        <div class="flex items-stretch bg-[#000000] h-24 mb-4">
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              onClick={clear}
-            >
-              AC
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="0"
-              onClick={updateCal}
-            >
-              0
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-gray-800 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              name="."
-              onClick={updateCal}
-            >
-              .
-            </button>
-          </div>
-
-          <div class="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
-            <button
-              class="rounded-full h-20 w-20 flex items-center bg-orange-500 justify-center shadow-lg hover:border-gray-500 focus:outline-none"
-              onClick={results}
-            >
-              =
-            </button>
-          </div>
+              <div className="row">
+                <input
+                  type="button"
+                  id="decimal"
+                  value="."
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  id="zero"
+                  value="0"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+                <input
+                  type="button"
+                  value="%"
+                  onClick={(e) => this.calcNumbers(e.target.value)}
+                />
+              </div>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default Cal;
+export default Calculator;
